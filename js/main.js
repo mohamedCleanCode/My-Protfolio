e8de18be6cc9d734d51a1f9a59c0062c09d000fd
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-11-03 15:16:48 +0200

    Edit js file
    add some logic

--- js/main.js
+++ js/main.js
@@ -7,3 +7,38 @@ window.onscroll = function () {
     scrollUp.style.display = "none";
   }
 };
+
+// Loop On Gallery
+
+// Switcher
+let buttonsOfTaps = document.querySelectorAll(".switcher li");
+let imgsOfTaps = document.querySelectorAll(".gallery img");
+let links = document.querySelectorAll(".pro .links a");
+
+links.forEach((a) => {
+  a.target = "_blank";
+});
+
+buttonsOfTaps.forEach((li) => {
+  li.addEventListener("click", manageActiveClass);
+  li.addEventListener("click", manageImgsFilter);
+});
+
+// Func Manage Active Class
+function manageActiveClass() {
+  buttonsOfTaps.forEach((li) => {
+    li.classList.remove("active");
+  });
+  this.classList.add("active");
+}
+
+// Func Manage Imgs Filter
+function manageImgsFilter() {
+  imgsOfTaps.forEach((img) => {
+    img.parentElement.style.display = "none";
+  });
+  document.querySelectorAll(this.dataset.cat).forEach((img) => {
+    img.parentElement.style.display = "block";
+    img.parentElement.style.cursor = "pointer";
+  });
+}
